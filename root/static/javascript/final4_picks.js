dc4e735a5712f59018b8132d769698b590b360f9
Mateu X. Hunter <hunter@missoula.org>
2010-03-28 21:19:30 -0400

    correct final 4 team movment javascript

--- root/static/javascript/final4_picks.js
+++ root/static/javascript/final4_picks.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
      	var game_number = parseInt(pattern_array[1]);
      	var team = pattern_array[2];
      	var next_game_number;
-     	if (game_number == 15 || game_number == 45) { next_game_number = 61 }
-     	else if (game_number == 60 || game_number == 30) { next_game_number = 62 }
+     	if (game_number == 15 || game_number == 30) { next_game_number = 61 }
+     	else if (game_number == 45 || game_number == 60) { next_game_number = 62 }
      	else if (game_number == 61 || game_number == 62) { next_game_number = 63 }
      	var new_id = 'w' + next_game_number + '-t' + team ;
 		var game = next_game_number;
