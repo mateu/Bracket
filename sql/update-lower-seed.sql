c3d6d05036c8337d533a0ef5ba8fd2175c66dfe3
m. hunter <hunter@406mt.org>
2024-03-22 16:09:10 -0600

    Update lower seed for region rounds

--- sql/update-lower-seed.sql
+++ sql/update-lower-seed.sql
@@ -11,3 +11,8 @@ update game
 set lower_seed = 1
 where game.id in (select * from lower_seed_games)
 ;
+
+update game
+set lower_seed = (get_winner_seed(game.id) > get_loser_seed(game.id))
+where game.round in (2, 3, 4)
+;
