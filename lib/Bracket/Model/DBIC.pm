bcf64b927852d24563805cb607edddf2a7fec75a
m. hunter <hunter@406mt.org>
2024-03-22 22:49:32 +0000

    Plug in lower seed updater

--- lib/Bracket/Model/DBIC.pm
+++ lib/Bracket/Model/DBIC.pm
@@ -23,7 +23,36 @@ sub update_points {
         sub {
             my $self = shift;
             my $dbh  = shift;
-            my $sth  = $dbh->prepare('delete from region_score;');
+            my $sth = $dbh->prepare('
+                with lower_seed_games as (
+                    select game.id game
+                    from team
+                        join pick on team.id = pick.pick
+                        join game on pick.game = game.id
+                    where pick.player = 1
+                        and game.round = 1
+                        and team.seed > 8
+                )
+                update game
+                set lower_seed = 1
+                where game.id in (select * from lower_seed_games)
+                ;
+            ');
+            $sth->execute;
+            my $sth = $dbh->prepare('
+                with games_played as (
+                    select pick.game
+                    from pick
+                    where pick.player = 1
+                )
+                update game
+                set lower_seed = (get_winner_seed(game.id) > get_loser_seed(game.id))
+                where game.round in (2, 3, 4)
+                and game.id in (select * from games_played)
+                ;
+            ');
+            $sth->execute;
+            $sth  = $dbh->prepare('delete from region_score;');
             $sth->execute;
             $sth = $dbh->prepare('
                 insert into region_score
