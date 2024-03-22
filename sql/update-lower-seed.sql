32562c4c3283094c5ce016dc1b41c58f1a7f65ba
m. hunter <hunter@406mt.org>
2024-03-22 16:22:12 -0600

    Restrict to games already played

--- sql/update-lower-seed.sql
+++ sql/update-lower-seed.sql
@@ -12,7 +12,13 @@ set lower_seed = 1
 where game.id in (select * from lower_seed_games)
 ;
 
+with games_played as (
+    select pick.game
+    from pick
+    where pick.player = 1
+)
 update game
 set lower_seed = (get_winner_seed(game.id) > get_loser_seed(game.id))
 where game.round in (2, 3, 4)
+and game.id in (select * from games_played)
 ;
