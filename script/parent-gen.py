a8e35e992ad15dd0eba8ce625161ba0f5633d57a
m. hunter <hunter@406mt.org>
2024-03-23 20:45:05 +0000

    Generate first round teams for each game

--- script/parent-gen.py
+++ script/parent-gen.py
@@ -1,4 +1,4 @@
-def generate_pairs(start, end):
+def generate_parent_games(start, end):
     """
     Generate parent games for region rounds 2-4
     Helps determine the seed of the winning team
@@ -12,7 +12,23 @@ def generate_pairs(start, end):
             for _ in range(1, 3):
                 z = z+1
                 yield (x+w*15, z+w*15)
-        w = w + 1
+        w = w+1
 
-for x, y in generate_pairs(9, 15):
+for x, y in generate_parent_games(9, 15):
     print(f"({x}, {y}),")
+print("---- done /w parent game ----")
+
+
+def generate_game_teams(start, end):
+    w = 0
+    for _ in range(0,4):
+        z = 0
+        for x in range(start, end + 1):
+            for _ in range(1, 3):
+                z = z+1
+                yield (x+w*15, z+w*16)
+        w = w+1
+
+for x, y in generate_game_teams(1, 8):
+    print(f"({x}, {y}),")
+print("---- done /w game teams ----")
