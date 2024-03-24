4f1685cb0e0c581012d65f9c7fbcc5b9ed5b5f47
m. hunter <hunter@406mt.org>
2024-03-24 16:23:13 +0000

    Plug in round out SQL

--- lib/Bracket/Model/DBIC.pm
+++ lib/Bracket/Model/DBIC.pm
@@ -23,6 +23,8 @@ sub update_points {
         sub {
             my $self = shift;
             my $dbh  = shift;
+
+	    # Note lower seeds
             my $sth = $dbh->prepare('
                 with lower_seed_games as (
                     select game.id game
@@ -52,6 +54,35 @@ sub update_points {
                 ;
             ');
             $sth->execute;
+
+	    # Update round out
+	    # Do for round 1
+            $dbh->prepare('
+              update team
+              set round_out = 1
+              where team.id in (select get_first_round_loser(game.id) from game where game.round = 1)
+              ;
+	    ');
+            $sth->execute;
+	    # Do for other rounds
+            $dbh->prepare('
+              with round_out_info as (
+                  select get_loser(game.id) as losing_team, game.round
+                  from team
+                      join pick on team.id = pick.pick
+                      join game on pick.game = game.id
+                  where pick.player = 1
+                  and round > 1
+              )
+              update team
+              inner join round_out_info roi on
+                  team.id = roi.losing_team
+              set round_out = roi.round
+              ;
+	    ');
+            $sth->execute;
+
+
             $sth  = $dbh->prepare('delete from region_score;');
             $sth->execute;
             $sth = $dbh->prepare('
--- sql/update-round-out.sql
+++ sql/update-round-out.sql
@@ -1,10 +1,10 @@
---- Do for round 1
+# Do for round 1
 update team
 set round_out = 1
 where team.id in (select get_first_round_loser(game.id) from game where game.round = 1)
 ;
 
---- Do for later rounds
+# Do for later rounds
 with round_out_info as (
     select get_loser(game.id) as losing_team, game.round
     from team
