7b53c9a480951e0aa05d45e0d538cd92eca2b712
m. hunter <hunter@406mt.org>
2024-04-02 13:58:55 -0600

    Combine some SQL

--- lib/Bracket/Model/DBIC.pm
+++ lib/Bracket/Model/DBIC.pm
@@ -26,33 +26,23 @@ sub update_points {
             my $dbh  = shift;
             my $start = time();
 
-	    # Note lower seeds
             my $sth = $dbh->prepare('
-                with lower_seed_games as (
-                    select game.id game
-                    from team
-                        join pick on team.id = pick.pick
-                        join game on pick.game = game.id
-                    where pick.player = 1
-                        and game.round = 1
-                        and team.seed > 8
-                )
-                update game
-                set lower_seed = 1
-                where game.id in (select * from lower_seed_games)
-                ;
-            ');
-            $sth->execute;
-            $sth = $dbh->prepare('
                 with games_played as (
-                    select pick.game
+                    select pick.game, team.seed
                     from pick
+                    join team
+                    on pick.pick = team.id
                     where pick.player = 1
                 )
                 update game
-                set lower_seed = (get_winner_seed(game.id) > get_loser_seed(game.id))
-                where game.round > 1
-                and game.id in (select * from games_played)
+                inner join games_played gp
+                on game.id = gp.game
+                set lower_seed = CASE
+                  WHEN game.round > 1 THEN (get_winner_seed(game.id) > get_loser_seed(game.id))
+                  # First round is deterministic based on seed
+                  ELSE gp.seed > 8
+                  END
+                  , game.winner = get_winner(game.id)
                 ;
             ');
             $sth->execute;
--- sql/update-lower-seed.sql
+++ sql/update-lower-seed.sql
@@ -11,20 +11,27 @@ with lower_seed_games as (
         and team.seed > 8
 )
 update game
-set lower_seed = 1
+set lower_seed = 1, game.winner = get_winner(game.id)
 where game.id in (select * from lower_seed_games)
 ;
 
 --- For other rounds we'll use the getter functions
 --- to find how who the teams involved are since
---- we don't know that up front like we for round 1
+--- we don't know that up front like we do for round 1
 with games_played as (
-    select pick.game
+    select pick.game, team.seed
     from pick
+    join team
+    on pick.pick = team.id
     where pick.player = 1
 )
 update game
-set lower_seed = (get_winner_seed(game.id) > get_loser_seed(game.id))
-where game.round > 1
-and game.id in (select * from games_played)
+inner join games_played gp
+on game.id = gp.game
+set lower_seed = CASE
+  WHEN game.round > 1 THEN (get_winner_seed(game.id) > get_loser_seed(game.id))
+  # First round is deterministic based on seed
+  ELSE gp.seed > 8
+  END
+  , game.winner = get_winner(game.id)
 ;
