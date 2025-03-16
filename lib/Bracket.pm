b75c84976604b84db2a4b57557a046b7667f4a17
m. hunter <hunter@406mt.org>
2025-03-16 11:28:23 -0600

    Prep for 2025

--- lib/Bracket.pm
+++ lib/Bracket.pm
@@ -1,7 +1,7 @@
 package Bracket;
 use Moose;
 
-our $VERSION = '1.17';
+our $VERSION = '1.18';
 use Catalyst::Runtime '5.80';
 
 use Catalyst qw/
@@ -67,7 +67,7 @@ Player brackets are compared to the perfect bracket for scoring purposes.
 
 =head1 AUTHOR
 
-Mateu X. Hunter 2008-2024
+Mateu X. Hunter 2008-2025
 hunter@missoula.org
 
 =head1 LICENSE
@@ -77,7 +77,7 @@ it under the same terms as Perl itself.
 
 =head1 COPYRIGHT
 
-Mateu X. Hunter 2008-2024
+Mateu X. Hunter 2008-2025
 
 =head1 LIMITATIONS
 
